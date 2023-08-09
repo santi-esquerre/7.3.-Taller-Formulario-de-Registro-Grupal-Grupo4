be568ca8d52b81bebe61050f7ca7974e4e2a91e0
santi-esquerre <bymaxxeryt@gmail.com>
2023-08-09 09:30:48 -0300

    Termina2

--- js/validacion.js
+++ js/validacion.js
@@ -9,23 +9,26 @@ const textboxEmail = document.getElementById("email");
 const textboxPassword1 = document.getElementById("password1");
 
 const textboxPassword2 = document.getElementById("password2");
+const checkbox = document.getElementById("terminos");
 
-if (
-  textboxNombre.value == "" ||
-  textboxEmail.value == "" ||
-  textboxApellido.value == "" ||
-  textboxPassword1.value.toString().length < 6 ||
-  textboxPassword2.value.toString().length < 6 ||
-  textboxPassword1.value !== textboxPassword2.value
-) {
-  regBtn.addEventListener("click", showAlertError());
-} else {
-  regBtn.addEventListener("click", showAlertSuccess());
-}
-function showAlertSuccess() {
-  document.getElementById("alert-sucess").classList.add("show");
-}
-
-function showAlertError() {
-  document.getElementById("alert-danger").classList.add("show");
-}
+regBtn.addEventListener("click", function showAlert() {
+  if (
+    textboxNombre.value == "" ||
+    textboxEmail.value == "" ||
+    textboxApellido.value == "" ||
+    textboxPassword1.value.toString().length < 6 ||
+    textboxPassword2.value.toString().length < 6 ||
+    textboxPassword1.value !== textboxPassword2.value ||
+    checkbox.checked == false
+  ) {
+    document.getElementById("alert-danger").classList.add("show");
+    setTimeout(function () {
+      document.getElementById("alert-danger").classList.remove("show");
+    }, 3000);
+  } else {
+    document.getElementById("alert-success").classList.add("show");
+    setTimeout(function () {
+      document.getElementById("alert-success").classList.remove("show");
+    }, 3000);
+  }
+});
